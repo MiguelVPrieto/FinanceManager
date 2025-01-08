f5ce1f25effc327f521d3361db3bb2ef0fa24fb4
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2025-01-07 21:09:43 -0300

    loadTransactions Function - 0.0.2

--- gui/database.py
+++ gui/database.py
@@ -5,9 +5,8 @@ file_path = r'C:\Users\migue\OneDrive\Área de Trabalho\Miguel\Programming\Finan
 excelFile = pd.ExcelFile(file_path, engine='openpyxl')
 accountsData = pd.read_excel(file_path, sheet_name='accounts-data', engine='openpyxl')
 
-transactionsNumber = accountsData.iloc[0, 7]
-formatted_transactionsNumber = str(transactionsNumber).zfill(4)
-
-transactionsData = pd.read_excel(file_path, sheet_name=str(formatted_transactionsNumber), engine='openpyxl')
-
-print(transactionsData.head())
+def loadTransactions(accountId):
+    transactionsID = accountsData.iloc[accountId, 7]
+    formattedTransactionsID = str(transactionsID).zfill(4)
+    transactionsData = pd.read_excel(file_path, sheet_name=str(formattedTransactionsID), engine='openpyxl')
+    return transactionsData
